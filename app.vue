4d6087f297bcf7a19cf5bd2e1c31a4c36a2e87e5
Noah Füreder <noah.fuereder@gmail.com>
2024-09-28 17:53:57 +0200

    enable speed insights

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+</script>
+
 <template>
   <Navbar class="bg-neutral-100/80 dark:bg-zinc-800/80 backdrop-blur" />
   <NuxtPage class="dark:bg-zinc-800" />
   <Footer />
+  <SpeedInsights />
 </template>
